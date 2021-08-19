7d38c00fd7ba0fd99bcbfa26a473e57961ff70d1
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2021-08-19 20:08:00 +0100

    Reverted some hud changes

--- code/rounds/Playing.cs
+++ code/rounds/Playing.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 public class Playing : BaseRound
 {
 	public override int RoundDuration => 30;
-	public override string RoundName => "Survive";
+	public override string RoundName => "Playing";
 
 	private static int numberOfPlayers;
 
--- code/ui/RoundTimer.cs
+++ code/ui/RoundTimer.cs
@@ -6,21 +6,15 @@ public class RoundTimer : Panel
 {
 	public Label TimerLabel;
 	private string time;
-	public Label RoundLabel;
 
 	public RoundTimer()
 	{
 		TimerLabel = Add.Label("0", "value");
-		RoundLabel = Add.Label( "Waiting For Players" );
-
-		TimerLabel.SetClass( "timer", true );
-		RoundLabel.SetClass( "round", true );
 	}
 
 	public override void Tick()
 	{
 		time = Game.Instance.Round.TimeLeftFormatted;
-		RoundLabel.Text = Game.Instance.Round.RoundName;
 		TimerLabel.Text = $"Time left: {time}";
 	}
 }
